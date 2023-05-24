c22db3f7c6069016532600138b2aa2e6ab309d12
Mpaula <mariapaulaflorezvargas11@gmail.com>
2023-05-24 10:14:16 -0500

    método para enviar datos de usuario a la base de datos

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -3,3 +3,26 @@ import 'package:firebase_core/firebase_core.dart';
 
 FirebaseFirestore db = FirebaseFirestore.instance;
 
+Future<String> addPeople(String nombre, String apellido, String password, String user) async{
+  
+  if(nombre !=null && apellido!=null && password!=null && user!=null){
+     if(password.length>=7){
+        await db.collection('usuarios').add(
+      {
+      "nombre": nombre,
+      "apellido": apellido,
+      "password": password,
+      "user": user,
+      }  
+      );
+     return "Ha creado su cuenta exitosamente";
+     }else{
+      return "La contraseña debe contener al menos 7 caracteres";
+     }
+  }else{
+    return "no puede ingresar campos vacíos";
+  }
+
+}
+
+
